0c45294395e0317f6b062589db16780878e038b4
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-09-05 15:00:04 +0100

    changed button to function

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,9 +1,5 @@
-import { Component } from "react";
-
-class ButtonComponent extends Component {
-  render() {
-    return <button style={{ width: "10rem", height: "3rem", fontSize: "2rem", marginTop: "1rem" }}>{this.props.label}</button>;
-  }
+const ButtonComponent = (label) => {
+    return <button style={{ width: "10rem", height: "3rem", fontSize: "2rem", marginTop: "1rem" }}>{label}</button>;
 }
 
 export default ButtonComponent;
