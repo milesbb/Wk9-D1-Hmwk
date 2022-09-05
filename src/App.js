2c4a293d37c92c28779f44eee74c678ae27739aa
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-09-05 17:25:54 +0100

    fixed variable use in components

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import ButtonComponent from "./components/ButtonComponent";
 import ImageComponent from "./components/ImageComponent";
 import InputComponent from "./components/InputComponent";
 
-// const name = "hello";
-// const image =
-//   "https://i.guim.co.uk/img/media/d143e03bccd1150ef52b8b6abd7f3e46885ea1b3/0_182_5472_3283/master/5472.jpg?width=1200&quality=85&auto=format&fit=max&s=d5a74a011c3fef1ad9c1c962721d221d";
-// const alt = "Forest";
-// const place = "Write";
+const name = "hello";
+const image =
+  "https://i.guim.co.uk/img/media/d143e03bccd1150ef52b8b6abd7f3e46885ea1b3/0_182_5472_3283/master/5472.jpg?width=1200&quality=85&auto=format&fit=max&s=d5a74a011c3fef1ad9c1c962721d221d";
+const alt = "Forest";
+const place = "Write";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Test Page</h1>
-        <ImageComponent src="https://i.guim.co.uk/img/media/d143e03bccd1150ef52b8b6abd7f3e46885ea1b3/0_182_5472_3283/master/5472.jpg?width=1200&quality=85&auto=format&fit=max&s=d5a74a011c3fef1ad9c1c962721d221d" alt="Forest" />
-        <InputComponent placeholder="write" />
-        <ButtonComponent label="hello" />
+        <ImageComponent src={image} alt={alt} />
+        <InputComponent placeholder={place} />
+        <ButtonComponent label={name} />
       </header>
     </div>
   );
--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,5 +1,5 @@
 const ButtonComponent = (label) => {
-    return <button style={{ width: "10rem", height: "3rem", fontSize: "2rem", marginTop: "1rem" }}>{label}</button>;
+    return <button style={{ width: "10rem", height: "3rem", fontSize: "2rem", marginTop: "1rem" }}>label</button>;
 }
 
 export default ButtonComponent;
